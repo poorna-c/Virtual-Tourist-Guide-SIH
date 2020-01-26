2d1cc9851c902da0b004e78592f9fd3ee6ca39ce
Poorna Chand <poornachandvanga091099@gmail.com>
2020-01-26 12:10:38 +0530

    Application and Server

---
+++ Android_Application/VirtualTouristGuide/app/src/main/java/com/iampc/virtualtouristguide/MainActivity.java
@@ -0,0 +1,200 @@
+package com.iampc.virtualtouristguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import java.lang.String;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+    Button btn;
+    EditText e_txt;
+    String url;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        e_txt = findViewById(R.id.text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView = findViewById(R.id.webview);
+                url = e_txt.getText().toString();
+                ((ViewManager)btn.getParent()).removeView(btn);
+                ((ViewManager)e_txt.getParent()).removeView(e_txt);
+
+                btn.setVisibility(View.INVISIBLE);
+                e_txt.setVisibility(View.INVISIBLE);
+
+
+
+                webView.loadUrl(url);
+
+                WebSettings mWebSettings = webView.getSettings();
+                mWebSettings.setJavaScriptEnabled(true);
+                mWebSettings.setSupportZoom(false);
+                mWebSettings.setAllowFileAccess(true);
+                mWebSettings.setAllowFileAccess(true);
+                mWebSettings.setAllowContentAccess(true);
+
+                webView.setWebChromeClient(new WebChromeClient()
+                {
+                    // For 3.0+ Devices (Start)
+                    // onActivityResult attached before constructor
+                    protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+                    {
+                        mUploadMessage = uploadMsg;
+                        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                        i.addCategory(Intent.CATEGORY_OPENABLE);
+                        i.setType("image/*");
+                        startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+                    }
+
+
+                    // For Lollipop 5.0+ Devices
+                    public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+                    {
+                        if (uploadMessage != null) {
+                            uploadMessage.onReceiveValue(null);
+                            uploadMessage = null;
+                        }
+
+                        uploadMessage = filePathCallback;
+
+                        Intent intent = fileChooserParams.createIntent();
+                        try
+                        {
+                            startActivityForResult(intent, REQUEST_SELECT_FILE);
+                        } catch (ActivityNotFoundException e)
+                        {
+                            uploadMessage = null;
+                            Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                            return false;
+                        }
+                        return true;
+                    }
+
+                    //For Android 4.1 only
+                    protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+                    {
+                        mUploadMessage = uploadMsg;
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.addCategory(Intent.CATEGORY_OPENABLE);
+                        intent.setType("image/*");
+                        startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+                    }
+
+                    protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+                    {
+                        mUploadMessage = uploadMsg;
+                        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                        i.addCategory(Intent.CATEGORY_OPENABLE);
+                        i.setType("image/*");
+                        startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (requestCode == REQUEST_SELECT_FILE)
+            {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        }
+        else if (requestCode == FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
